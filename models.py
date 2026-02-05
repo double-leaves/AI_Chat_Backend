dc584ca56b2fc068a02b5708934f0929fb790025
double-leaves <3260039938@qq.com>
2026-02-05 17:36:03 +0800

    "初步完善了模型"

--- models.py
+++ models.py
@@ -4,20 +4,56 @@ from sqlmodel import SQLModel, Field
 
 # 用户
 class UserBase(SQLModel):
-    username: str
+    username: str = Field(index=True, unique=True)
+
+
+class User(UserBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    hashed_password: str
 
 
 class UserCreate(UserBase):
-    password: str = Field(min_length=8, max_length=24)  # 密码长度限制
-
-
-class UserData(UserBase):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    password: str = Field(min_length=8, max_length=72)  # 密码长度限制
 
 
 class UserRead(UserBase):
+    id: int
+
+
+# 会话表
+class ChatSessionBase(SQLModel):
+    title: str
+
+
+class ChatSession(ChatSessionBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
 
 
+class ChatSessionCreate(ChatSessionBase):
+    pass
 
 
+class ChatSessionRead(ChatSessionBase):
+    id: int
+    user_id: int
+
+
+# 消息表
+class ChatMessageBase(SQLModel):
+    content: str
+
+
+class ChatMessage(ChatMessageBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+    session_id: Optional[int] = Field(default=None, foreign_key="chatsession.id")
+    judge: str
+
+
+class ChatMessageCreate(ChatMessageBase):
+    pass
+
+
+class ChatMessageRead(ChatMessageBase):
+    id: int
+    judge: str
