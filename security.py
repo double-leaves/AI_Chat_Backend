129e5787508c0107901450b68b867a0b4712fddc
double-leaves <3260039938@qq.com>
2026-02-06 23:55:19 +0800

    实现鉴权功能

--- security.py
+++ security.py
@@ -1,8 +1,12 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Union
 import jwt
+from sqlmodel import Session, select
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
-
+from database import get_session
+from models import User
 
 #  配置
 SECRET_KEY = "你自己随便写一串复杂的乱码"
@@ -11,24 +15,65 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 # 密码哈希
-def get_password_hash(secret: str):
-    return
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
 
 
-def verify_password(secret: str, hashed: str):
-    if get_password_hash(secret) == hashed:
-        return True
-    return False
+# 判断密码是否正确
+def verify_password(secret: str, hashed: str) -> bool:
+    return pwd_context.verify(secret, hashed)
 
-# JWT制造
+
+# 创建JWT
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
 
-    end_time = datetime.now() + expires_delta
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
-    to_encode["exp"] = end_time
+    to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode()
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+# 鉴权（Authentication）
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    session: Session = Depends(get_session)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    # 查库找人
+    statement = select(User).where(User.username == username)
+    user = session.exec(statement)
+
+    return user
+
+
+
+
+
